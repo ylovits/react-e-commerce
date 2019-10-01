f7c873a481c76a1c1964ad2e270b3afa7c7aef82
Michail Roditis <ylovits@gmail.com>
2019-10-01 16:19:06 +0200

    Category page with hooks
    Made a category page as functional component with hhoks for state and  basic product components

---
+++ src/components/ProductTeaser/ProductTeaser.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductTeaser = ({ product }) => {
+  return <div className="col-md-2">{product.name}</div>;
+};
+
+export default ProductTeaser;
--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ProductTeaser from "../../components/ProductTeaser/ProductTeaser.js";
+import NotFound from "../NotFound/NotFound";
 
-const Category = props => {
-  return <div>CategoryPage {props.match.params.categoryId}</div>;
+const Category = ({ match }) => {
+  const [products, setProducts] = useState([]);
+  const productsjson = require("../../fakeapi/products.json");
+
+  useEffect(() => {
+    var categoryProds = productsjson.filter(product =>
+      product.category
+        .toLowerCase()
+        .includes(match.params.categoryId.toLowerCase())
+    );
+    setProducts(categoryProds);
+    // eslint-disable-next-line
+  }, []);
+
+  const categExists = categName => {
+    let i = null;
+    for (i = 0; productsjson.length > i; i += 1) {
+      if (productsjson[i].category.toLowerCase() === categName) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (categExists(match.params.categoryId.toLowerCase())) {
+    return (
+      <div className="container banners">
+        <div className="row">
+          {products.map(product => (
+            <ProductTeaser key={product.MPN} product={product} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return <NotFound />;
+  }
 };
 
 export default Category;
